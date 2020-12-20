1d2149a8e547224a78c91d2ac822c963d07e416f
paullongtan <47477883+paullongtan@users.noreply.github.com>
2020-12-21 01:10:18 +0800

    Update UI_new.py

--- UI_new.py
+++ UI_new.py
@@ -14,7 +14,7 @@ class LoginWindow(tk.Frame):
     
     def createWidgets(self):
         f1 = tkFont.Font(size = 24, family = "jf open 粉圓 1.1")
-        f3 = tkFont.Font(size = 16, family = "jf open 粉圓 1.1")
+        f3 = tkFont.Font(size = 16, family = "標楷體")
 
         # self.cvsMain = tk.Canvas(self, width = 400, height = 300, bg = "white")
         self.lblExp = tk.Label(self, text = "歡迎使用修課檢驗系統，請在下方輸入您的姓名", height = 1, width = 40, font = f3)
@@ -58,10 +58,11 @@ class SignUpWindow(tk.Frame):
     def __init__(self, master, user):
         self.master = master
         self.user = user
+        self.master.geometry('+500+400')
         self.createWindow()
 
     def createWindow(self):
-        f3 = tkFont.Font(size = 16, family = "jf open 粉圓 1.1")
+        f3 = tkFont.Font(size = 16, family = "標楷體")
 
         self.lblSignExp = tk.Label(self.master, text = "第一次見到您，讓我們來為您建立你專屬的修課紀錄！", height = 1, width = 40, font = f3)
         self.lblSignName = tk.Label(self.master, text = "姓名:", height = 1, width = 6, font = f3)
@@ -95,10 +96,63 @@ class SignUpWindow(tk.Frame):
         
         tk.messagebox.showinfo('註冊','已替您建立您的帳戶')
         self.master.destroy()
+        root.destroy()
 
+class MainWindow(tk.Frame):
+
+    def __init__(self, master):
+        self.master = master
+        self.createWindow()
+    
+    def createWindow(self):
+        self.userData = tk.LabelFrame(text="PERSONAL DATA", font="TimesNewRoman 16 bold")
+        self.userData.config(height=220, width=400, relief="flat", bd=10)
+        self.userData.config(highlightbackground="#888888", highlightthickness=5)
+        self.userData.place(x=25, y=25)
+
+        self.userName = tk.Label(self.userData, text="● 姓名：吳承翰", font="標楷體")
+        self.userName.place(x=10, y=10)
+        self.userDpt = tk.Label(self.userData, text="● 系級：經濟系 大一", font="標楷體")
+        self.userDpt.place(x=10, y=35)
+        self.creditEarned = tk.Label(self.userData, text="● 已修習學分數：", font="標楷體")
+        self.creditEarned.place(x=10, y=60)
+        self.creditLack = tk.Label(self.userData, text="● 尚需學分數：", font="標楷體")
+        self.creditLack.place(x=10, y=110)
+
+        self.courseTable = tk.LabelFrame()
+        self.courseTable.config(height=675, width=540, relief="flat", bd=0)
+        self.courseTable.config(highlightbackground="#888888", highlightthickness=5)
+        self.courseTable.place(x=440, y=25)
+
+        days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        lessonCode = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "B", "C", "D"]
+        curriculum = []
+        for i in range(112):
+            if i < 16:
+                if int(i%16) == 0:
+                    curriculum.append(tk.Label(self.courseTable, text=""))
+                else:
+                    curriculum.append(tk.Label(self.courseTable, text="%s"%lessonCode[i - 1], fg="#000000"))
+            elif int(i%16) == 0:
+                curriculum.append(tk.Label(self.courseTable, text="%s"%days[int(i/16) - 1], fg="#000000"))
+            else:
+                curriculum.append(tk.Button(self.courseTable, text=""))
+            
+            curriculum[i].config(height=2, width=11)
+            curriculum[i].grid(row=int(i%16), column=int(i/16))
 
 root = tk.Tk()
-root.config(background="yellow")
+root.geometry('+500+400')  
 app = LoginWindow(root)
 app.master.title("台大修課檢驗系統")
 app.mainloop()
+
+win = tk.Tk()
+win.geometry("1400x780+60+10")
+main = MainWindow(win)
+win.resizable(0, 0)
+win.title("Course Selection Supporting System")
+
+
+
+win.mainloop()
